33fd3a62787f78856f7faa765196b140ad1eae73
v_octxiao <v_octxiao@tencent.com>
2025-06-05 15:07:49 +0800

    安装路由，ant组件库

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+// 解决antd v5在react19中使用的问题
+import '@ant-design/v5-patch-for-react-19';
 import 'normalize.css';
 import './styles/index.less';
 import App from './App.tsx';
